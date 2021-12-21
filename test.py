1da0a67543ca345e604cc2b77523856db8402ad4
ITJaylon <1375637251@qq.com>
2021-12-21 17:27:45 +0800

    GPU运行版本，bert+GRU

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 import torch
-a = torch.randn(2,3,4)
-print(a[:,1:2,:])
+from torch import nn
+a = torch.tensor([[[1.0,2.0,3.0],[1.0,2.0,3.0]],[[2.0,2.0,3.0],[2.0,2.0,3.0]]])
+b = torch.tensor([[0.5],[0.4]])
+print(torch.sum(a, dim=1))
